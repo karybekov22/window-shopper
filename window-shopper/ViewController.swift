393244226b97569cc9acc916bfd1c24f4c94d46e
Nursultan Karybekov <n.karybekov22@gmail.com>
2021-02-23 13:43:05 +0600

    Initial Commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Nursultan Karybekov on 23/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
