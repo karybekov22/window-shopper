e207bdb4c7f3172d1e9009d45105c118e84d07d2
Nursultan Karybekov <n.karybekov22@gmail.com>
2021-03-03 02:02:16 +0600

    test

--- window-shopperTests/window_shopperTests.swift
+++ window-shopperTests/window_shopperTests.swift
@@ -18,7 +18,7 @@ class window_shopperTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
+        // This is an example of a functional test case. test
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
